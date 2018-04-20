a80849d1002c40276e1f7ee64dedb895b98a4fc9
Mrinal <waliamrinal@gmail.com>
2018-04-20 12:58:23 +0530

    python calendar program

---
+++ calendar.py
@@ -0,0 +1,11 @@
+#python program to make a calendar
+
+#import calendar module
+import calendar
+
+#input the data - year and month 
+year = 1999
+month = 2
+
+#make a calendar
+print(calendar.month(year, month))
