db8459722cc15861c743a4cb83fd462ba8181219
Mrinal <waliamrinal@gmail.com>
2018-04-26 14:39:34 +0530

    add these programs

---
+++ calender.py
@@ -0,0 +1,9 @@
+#python program to make a calendar
+
+import calendar
+
+#input the year and month
+year = int(input("ENTER THE YEAR\t"))
+month = int(input('ENTER THE MONTH\t'))
+
+print(calendar.month(year,month))
